caaa4851fb67965365c3002f3c519cf289bdfdf2
fahmiaz411 <fahmiaziz884@gmail.com>
2023-03-04 18:09:26 +0700

    read config from env

--- app/main.go
+++ app/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 
 	"github.com/fahmiaz411/devcode/config/database"
+	"github.com/fahmiaz411/devcode/helper/constant"
 	_activityHandler "github.com/fahmiaz411/devcode/modules/activity/delivery"
 	_activityRepo "github.com/fahmiaz411/devcode/modules/activity/repository"
 	_activityUsecase "github.com/fahmiaz411/devcode/modules/activity/usecase"
@@ -17,14 +20,29 @@ import (
 
 func main() {
 	app := fiber.New()
+
+	mysqlPort, _ := strconv.Atoi(os.Getenv("MYSQL_PORT"))
+	if mysqlPort == constant.ZeroValue {
+		mysqlPort = 3306
+	}
 	
 	db := database.NewMysqlDB(database.MysqlConfig{
-		DatabaseName: "devcode",
-		Username: "root",
-		Password: "1234",
-		Host: "localhost",
-		Port: 3306,
+		DatabaseName: os.Getenv("MYSQL_DBNAME"),
+		Username: os.Getenv("MYSQL_USER"),
+		Password: os.Getenv("MYSQL_PASSWORD"),
+		Host: os.Getenv("MYSQL_HOST"),
+		Port: mysqlPort,
 	})
+
+	// Dev
+
+	// db := database.NewMysqlDB(database.MysqlConfig{
+	// 	DatabaseName: "devcode",
+	// 	Username: "root",
+	// 	Password: "1234",
+	// 	Host: "localhost",
+	// 	Port: 3306,
+	// })
 
 	timeout := time.Duration(1 * time.Minute)
 
